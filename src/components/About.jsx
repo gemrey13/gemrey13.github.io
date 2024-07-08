0f802d52f242d961aab901654125ce57cbd187b4
gemrey13 <gemreyranola@gmail.com>
2024-07-08 16:58:47 +0800

    ci: added about me

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,7 +1,29 @@
+import AboutImage from "../assets/about.jpg";
+import { ABOUT_TEXT } from "../constants/index.js";
+
 const About = () => {
   return (
     <div className="borde-b border-neutral-900 pb-4">
-      <h2 className="">About Me</h2>
+      <h2 className="my-20 text-center text-4xl">
+        About
+        <span className="text-neutral-500"> Me</span>
+      </h2>
+      <div className="flex flex-wrap items-center">
+        <div className="w-full lg:w-1/2 lg:p-8">
+          <div className="flex items-center justify-center">
+            <img
+              className="w-[70%]  rounded-2xl"
+              src={AboutImage}
+              alt="Gem Coding"
+            />
+          </div>
+        </div>
+        <div className="w-full lg:w-1/2">
+          <div className="flex justify-center lg:justify-start">
+            <p className="my-2 max-w-xl py-6">{ABOUT_TEXT}</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
