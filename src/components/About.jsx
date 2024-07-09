ca8dee9c9899aafc5efb7439751c07fe6fef895c
gemrey13 <gemreyranola@gmail.com>
2024-07-09 11:10:46 +0800

    ci: added animation for about section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,5 +1,6 @@
 import AboutImage from "../assets/about.jpg";
 import { ABOUT_TEXT } from "../constants/index.js";
+import { motion } from "framer-motion";
 
 const About = () => {
   return (
@@ -9,7 +10,12 @@ const About = () => {
         <span className="text-neutral-500"> Me</span>
       </h2>
       <div className="flex flex-wrap items-center">
-        <div className="w-full lg:w-1/2 lg:p-8">
+        <motion.div
+          whileInView={{ opacity: 1, x: 0 }}
+          initial={{ opacity: 0, x: -100 }}
+          transition={{ duration: 0.5 }}
+          className="w-full lg:w-1/2 lg:p-8"
+        >
           <div className="flex items-center justify-center">
             <img
               className="w-[70%]  rounded-2xl"
@@ -17,12 +23,17 @@ const About = () => {
               alt="Gem Coding"
             />
           </div>
-        </div>
-        <div className="w-full lg:w-1/2">
+        </motion.div>
+        <motion.div
+          whileInView={{ opacity: 1, x: 0 }}
+          initial={{ opacity: 0, x: 100 }}
+          transition={{ duration: 0.5 }}
+          className="w-full lg:w-1/2"
+        >
           <div className="flex justify-center lg:justify-start">
             <p className="my-2 max-w-xl py-6">{ABOUT_TEXT}</p>
           </div>
-        </div>
+        </motion.div>
       </div>
     </div>
   );
