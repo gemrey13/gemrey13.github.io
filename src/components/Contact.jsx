97073b51da453ef113c4d02f24f3bd94f9607b85
gemrey13 <gemreyranola@gmail.com>
2024-07-09 11:32:00 +0800

    ci: added animation for contact section

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,14 +1,35 @@
 import { CONTACT } from "../constants";
+import { motion } from "framer-motion";
 
 const Contact = () => {
   return (
     <div className="border-b border-neutral-800 pb-20">
-      <h2 className="my-10 text-center text-4xl">Get in Touch</h2>
+      <motion.h2
+        whileInView={{ opacity: 1, y: 0 }}
+        initial={{ opacity: 0, y: -100 }}
+        transition={{ duration: 0.5 }}
+        className="my-10 text-center text-4xl"
+      >
+        Get in Touch
+      </motion.h2>
       <div className="text-center tracking-tighter">
-        <p className="my-4">{CONTACT.address}</p>
-        <a href="#" className="border-b">
+        <motion.p
+          whileInView={{ opacity: 1, x: 0 }}
+          initial={{ opacity: 0, x: -100 }}
+          transition={{ duration: 1 }}
+          className="my-4"
+        >
+          {CONTACT.address}
+        </motion.p>
+        <motion.a
+          whileInView={{ opacity: 1, x: 0 }}
+          initial={{ opacity: 0, x: 200 }}
+          transition={{ duration: 1 }}
+          href="#"
+          className="border-b"
+        >
           {CONTACT.email}
-        </a>
+        </motion.a>
       </div>
     </div>
   );
