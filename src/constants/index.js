dd78943646e9f9427d1ef153858af03776cc7801
gemrey13 <gemreyranola@gmail.com>
2024-07-09 09:43:05 +0800

    ci: added Experience section

--- src/constants/index.js
+++ src/constants/index.js
@@ -10,32 +10,18 @@ export const ABOUT_TEXT = `I am an aspiring developer with junior-level experien
 
 export const EXPERIENCES = [
     {
-        year: "2023 - Present",
-        role: "Senior Full Stack Developer",
-        company: "Google Inc.",
-        description: `Led a team in developing and maintaining web applications using JavaScript, React.js, and Node.js. Implemented RESTful APIs and integrated with MongoDB databases. Collaborated with stakeholders to define project requirements and timelines.`,
-        technologies: ["Javascript", "React.js", "Next.js", "mongoDB"],
+        year: "March 2024 - Present",
+        role: "Software Developer Intern",
+        company: "Cooperative Bank of Quezon Province",
+        description: `Led a team in developing and maintaining web applications using Django, Postgresql, and TailwindCSS. Developed two systems: an FFE inventory system and a voting system for the cooperative. Collaborated with stakeholders to define project requirements and timelines.`,
+        technologies: ["Django", "TailwindCSS", "Postgresql", "HTML"],
     },
     {
-        year: "2022 - 2023",
-        role: "Frontend Developer",
-        company: "Adobe",
-        description: `Designed and developed user interfaces for web applications using Next.js and React. Worked closely with backend developers to integrate frontend components with Node.js APIs. Implemented responsive designs and optimized frontend performance.`,
-        technologies: ["HTML", "CSS", "Vue.js", "mySQL"],
-    },
-    {
-        year: "2021 - 2022",
-        role: "Full Stack Developer",
-        company: "Facebook",
-        description: `Developed and maintained web applications using JavaScript, React.js, and Node.js. Designed and implemented RESTful APIs for data communication. Collaborated with cross-functional teams to deliver high-quality software products on schedule.`,
-        technologies: ["Python", "Svelte", "Three.js", "Postgres"],
-    },
-    {
-        year: "2020 - 2021",
-        role: "Software Engineer",
-        company: "Paypal",
-        description: `Contributed to the development of web applications using JavaScript, React.js, and Node.js. Managed databases and implemented data storage solutions using MongoDB. Worked closely with product managers to prioritize features and enhancements.`,
-        technologies: ["Ruby", "Rails", "PHP", "Sqlite"],
+        year: "December 2020 - February 2021",
+        role: "IT Support Intern",
+        company: "RA Insurance Services",
+        description: `I managed printing tasks, fixed computers, and assisted co-workers with technical issues. I provided troubleshooting support to identify and resolve hardware and software problems. This role enhanced my problem-solving skills and deepened my understanding of IT support in a professional setting.`,
+        technologies: ["WindowsOS", "Microsoft Office Suite", "Computers", "TeamViewer"],
     },
 ];
 
