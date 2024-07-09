349e2769647f36b17226a3b0414bc36db323c020
gemrey13 <gemreyranola@gmail.com>
2024-07-09 11:07:24 +0800

    ci: added animation for hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,29 +1,67 @@
 import { HERO_CONTENT, HERO_CONTENT2 } from "../constants/index.js";
 import gemPic from "../assets/gemPic.jpg";
+import { motion } from "framer-motion";
+
+const container = (delay) => ({
+  hidden: { x: -100, opacity: 0 },
+  visible: {
+    x: 0,
+    opacity: 1,
+    transition: { duration: 0.5, delay: delay },
+  },
+});
+
 const Hero = () => {
   return (
     <div className="border-b border-neutral-900 pb-4 lg:mb-35">
       <div className="flex flex-wrap">
         <div className="w-full lg:w-1/2">
           <div className="flex flex-col items-center lg:items-start">
-            <h1 className="pb-16 text-6xl font-thin tracking-tight lg:mt-16 lg:text-6xl">
+            <motion.h1
+              variants={container(0)}
+              initial="hidden"
+              animate="visible"
+              className="pb-16 text-6xl font-thin tracking-tight lg:mt-16 lg:text-6xl"
+            >
               Gem Rey Rañola
-            </h1>
-            <span className="bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 bg-clip-text text-2xl lg:text-3xl tracking-tight text-transparent">
+            </motion.h1>
+            <motion.span
+              variants={container(0.5)}
+              initial="hidden"
+              animate="visible"
+              className="bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 bg-clip-text text-2xl lg:text-3xl tracking-tight text-transparent"
+            >
               Freelance Software Developer
-            </span>
-            <p className="mt-2 max-w-xl pt-6 font-light tracking-tighter">
+            </motion.span>
+            <motion.p
+              variants={container(1)}
+              initial="hidden"
+              animate="visible"
+              className="mt-2 max-w-xl pt-6 font-light tracking-tighter"
+            >
               {HERO_CONTENT}
-            </p>
+            </motion.p>
             <br />
-            <p className="mb-2 max-w-xl pb-6 font-light tracking-tighter">
+            <motion.p
+              variants={container(1)}
+              initial="hidden"
+              animate="visible"
+              className="mb-2 max-w-xl pb-6 font-light tracking-tighter"
+            >
               {HERO_CONTENT2}
-            </p>
+            </motion.p>
           </div>
         </div>
         <div className="w-full lg:w-1/2 lg:p-8">
           <div className="flex justify-center">
-            <img className="w-[90%]" src={gemPic} alt="Gem Rey Rañola" />
+            <motion.img
+              initial={{ x: 100, opacity: 0 }}
+              animate={{ x: 0, opacity: 1 }}
+              transition={{ duration: 1, delay: 1.2 }}
+              className="w-[90%]"
+              src={gemPic}
+              alt="Gem Rey Rañola"
+            />
           </div>
         </div>
       </div>
