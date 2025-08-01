43621b6db62c786176586e390964ad436f34189e
gemrey13 <gemreyranola@gmail.com>
2025-08-01 13:51:44 +0800

    added navbar

---
+++ src/components/Home/HeroSection.tsx
@@ -0,0 +1,10 @@
+const HeroSection = () => {
+  return (
+    <section>
+      <div>asd</div>
+      <div>photos</div>
+    </section>
+  );
+};
+
+export default HeroSection;
--- src/components/ui/Navbar.tsx
+++ src/components/ui/Navbar.tsx
@@ -1,9 +1,36 @@
+import { Link } from "react-router-dom";
+import FigmaIcon from "../../assets/icon/Figma Icon.png"
+
 const Navbar = () => {
   return (
-    <div className="bg-red-600">
-      asd
-    </div>
-  )
-}
+    <nav className="sticky top-0 mt-5 z-50 w-[30%] mx-auto rounded-full border-secondary border font-semibold">
+      <div className="flex justify-between mx-3 items-center h-12 my-1">
+        <Link to="/" className="rounded-full bg-[#572F2F] h-10 w-10 text-white  flex items-center justify-center">
+          GR.
+        </Link>
+        <ul className="flex space-x-10 text-gray-700">
+          <li>
+            <Link to="#" className="hover:text-blue-600">
+              Projects
+            </Link>
+          </li>
+          <li>
+            <Link to="#" className="hover:text-blue-600">
+              About
+            </Link>
+          </li>
+          <li>
+            <Link to="#" className="hover:text-blue-600">
+              Résumé
+            </Link>
+          </li>
+        </ul>
+        <Link to="https://www.figma.com/proto/rFT0FwQwJBW9WQf64oAmJc/E-Portfolio?node-id=74-156&p=f&t=qaIetIH43a5PPoTj-1&scaling=scale-down&content-scaling=fixed&page-id=0%3A1" className="rounded-full bg-gray-300 h-10 w-10 text-white flex items-center justify-center">
+          <img src={FigmaIcon} alt="Figma icon with colors" />
+        </Link>
+      </div>
+    </nav>
+  );
+};
 
-export default Navbar
+export default Navbar;
