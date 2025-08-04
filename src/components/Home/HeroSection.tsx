06e866513d54395ea8280d7bcbc0ced657c14153
gemrey13 <gemreyranola@gmail.com>
2025-08-04 08:56:01 +0800

    Refactor code structure for improved readability and maintainability

--- src/components/Home/HeroSection.tsx
+++ src/components/Home/HeroSection.tsx
@@ -1,30 +1,46 @@
 import { Link } from "react-router-dom";
 import GradPic from "../../assets/photos/Gem Grad Pic.png";
-import { FaFacebook, FaInstagram, FaLinkedin, FaTiktok, FaWhatsapp } from "react-icons/fa";
+import {
+  FaFacebook,
+  FaInstagram,
+  FaLinkedin,
+  FaTiktok,
+  FaWhatsapp,
+} from "react-icons/fa";
 
 const HeroSection = () => {
   return (
-      <section className="mt-20 md:mt-0 grid md:grid-cols-2 min-h-screen">
-
+    <section className="grid md:mt-0 md:grid-cols-2 h-screen">
       {/* Text Section */}
-      <div className="h-[10vh] md:h-auto flex flex-col justify-center space-y-6 ">
-        <h1 className="text-3xl md:text-5xl font-bold">
+      <div className="flex flex-col justify-center space-y-6 mx-5 md:mx-14">
+        <h1 className="text-3xl font-bold md:text-5xl">
           Hello, I'm Gem. <br /> Hire Me!
         </h1>
 
         <p className="text-secondary">
-          I'm a recent Bachelor in Information Technology graduate with a specialization in web
-          development.
+          I'm a recent Bachelor in Information Technology graduate with a
+          specialization in web development.
         </p>
-        <Link to="" target="_blank" rel="noopener noreferrer" className="text-link underline font-bold text-xl md:text-2xl">Download My Résumé</Link>
+        <Link
+          to="/Gem Rey Rañola.pdf"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="text-link text-xl font-bold underline md:text-2xl"
+        >
+          Download My Résumé
+        </Link>
       </div>
 
       {/* Image Section */}
-      <div className="h-[50vh] md:h-screen relative overflow-hidden">
-        <img src={GradPic} alt="Gem graduation" className="h-full w-full object-cover" />
+      <div className="relative h-[50vh] overflow-hidden md:h-screen">
+        <img
+          src={GradPic}
+          alt="Gem graduation"
+          className="h-full w-full object-cover"
+        />
 
         {/* Social Icons */}
-        <div className="absolute bottom-5 right-5 flex space-x-6 text-white text-2xl md:text-3xl">
+        <div className="absolute right-5 bottom-5 flex space-x-6 text-2xl text-white md:text-3xl">
           <FaFacebook />
           <FaInstagram />
           <FaLinkedin />
