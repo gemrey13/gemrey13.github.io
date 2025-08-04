084bb065afbe0aaf1585368dfcbff78b4090187b
gemrey13 <gemreyranola@gmail.com>
2025-08-04 08:36:06 +0800

    Navbar design

--- src/components/Home/HeroSection.tsx
+++ src/components/Home/HeroSection.tsx
@@ -1,23 +1,26 @@
+import { Link } from "react-router-dom";
 import GradPic from "../../assets/photos/Gem Grad Pic.png";
 import { FaFacebook, FaInstagram, FaLinkedin, FaTiktok, FaWhatsapp } from "react-icons/fa";
 
 const HeroSection = () => {
   return (
-    <section className="grid md:grid-cols-2 min-h-screen">
+      <section className="mt-20 md:mt-0 grid md:grid-cols-2 min-h-screen">
+
       {/* Text Section */}
-      <div className="h-[30vh] md:h-auto flex flex-col justify-center space-y-6 px-8 m-6 md:m-11">
-        <h1 className="text-4xl font-bold">
+      <div className="h-[10vh] md:h-auto flex flex-col justify-center space-y-6 ">
+        <h1 className="text-3xl md:text-5xl font-bold">
           Hello, I'm Gem. <br /> Hire Me!
         </h1>
-        <p>
+
+        <p className="text-secondary">
           I'm a recent Bachelor in Information Technology graduate with a specialization in web
           development.
         </p>
-        <p>Download My Résumé</p>
+        <Link to="" target="_blank" rel="noopener noreferrer" className="text-link underline font-bold text-xl md:text-2xl">Download My Résumé</Link>
       </div>
 
       {/* Image Section */}
-      <div className="h-[70vh] md:h-screen relative overflow-hidden">
+      <div className="h-[50vh] md:h-screen relative overflow-hidden">
         <img src={GradPic} alt="Gem graduation" className="h-full w-full object-cover" />
 
         {/* Social Icons */}
--- src/components/ui/Navbar.tsx
+++ src/components/ui/Navbar.tsx
@@ -3,15 +3,16 @@ import FigmaIcon from "../../assets/icon/Figma Icon.png";
 
 const Navbar = () => {
   return (
-    <nav className="fixed top-4 left-1/2 transform -translate-x-1/2 z-50 w-[90%] md:w-1/2 lg:w-[30%] rounded-full border border-secondary bg-white font-semibold text-sm md:text-base">
-      <div className="flex justify-between items-center mx-4 md:mx-5 h-11 my-1">
+    <nav className=" border-secondary/20 bg-white/70  shadow-md backdrop-blur backdrop-saturate-150 sticky top-2 z-50 mx-auto w-[90%] rounded-full border  text-sm font-semibold md:fixed md:top-4 md:left-1/2 md:w-1/2 md:-translate-x-1/2 md:transform md:text-base lg:w-[30%]">
+      <div className="mx-4 my-1 flex h-11 items-center justify-between md:mx-5">
         <Link
           to="/"
-          className="rounded-full bg-[#572F2F] h-9 w-9 text-white flex items-center justify-center">
+          className="flex h-9 w-9 items-center justify-center rounded-full bg-[#572F2F] text-white"
+        >
           GR.
         </Link>
 
-        <ul className="flex space-x-5 md:space-x-10 text-gray-700">
+        <ul className="flex space-x-5 text-gray-700 md:space-x-10">
           <li>
             <Link to="#" className="hover:text-blue-600">
               Projects
@@ -31,8 +32,13 @@ const Navbar = () => {
 
         <Link
           to="https://www.figma.com/proto/rFT0FwQwJBW9WQf64oAmJc/E-Portfolio?node-id=74-156&p=f&t=qaIetIH43a5PPoTj-1&scaling=scale-down&content-scaling=fixed&page-id=0%3A1"
-          className="rounded-full bg-gray-300 h-9 w-9 md:h-8 md:w-8 lg:h-9 lg:w-9 flex items-center justify-center">
-          <img src={FigmaIcon} alt="Figma icon" className="h-9 w-9 md:h-8 md:w-8 lg:h-9 lg:w-9" />
+          className="flex h-9 w-9 items-center justify-center rounded-full bg-gray-300 md:h-8 md:w-8 lg:h-9 lg:w-9"
+        >
+          <img
+            src={FigmaIcon}
+            alt="Figma icon"
+            className="h-9 w-9 md:h-8 md:w-8 lg:h-9 lg:w-9"
+          />
         </Link>
       </div>
     </nav>
