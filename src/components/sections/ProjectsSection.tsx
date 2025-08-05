8347b499851954fcbfc8218dc218f638fffa79e5
gemrey13 <gemreyranola@gmail.com>
2025-08-05 11:55:31 +0800

    Add ProjectsSection component and integrate it into Home page layout

---
+++ src/components/sections/ProjectsSection.tsx
@@ -0,0 +1,18 @@
+import Title from "../ui/Title"
+
+const ProjectsSection = () => {
+  return (
+    <section className="mt-14 mx-5 md:mx-16">
+      <Title
+        header="Projects"
+        linkTitle="Explore my work"
+        internalLink="/projects"
+      />
+      <div className="font-droid text-secondary my-6 md:mx-4 md:my-10">
+  
+      </div>
+    </section>
+  )
+}
+
+export default ProjectsSection
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,7 @@
 import AboutMeSection from "../components/sections/AboutMeSection";
 import ExperienceSection from "../components/sections/ExperienceSection";
 import HeroSection from "../components/sections/HeroSection";
+import ProjectsSection from "../components/sections/ProjectsSection";
 import QuoteSection from "../components/sections/QuoteSection";
 
 const Home = () => {
@@ -10,6 +11,8 @@ const Home = () => {
       <AboutMeSection />
       <QuoteSection />
       <ExperienceSection />
+      <hr className="mx-5 border-gray-300 md:mx-16" />
+      <ProjectsSection />
     </>
   );
 };
