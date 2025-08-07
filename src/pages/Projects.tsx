ded4dfb42290b42e5d43d4cb4c2dc4295263ceaa
gemrey13 <gemreyranola@gmail.com>
2025-08-07 09:30:51 +0800

    Refactor Projects page to replace ProjectsSection with ProjectItems for improved structure

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -1,10 +1,11 @@
-import ProjectsSection from "../components/sections/ProjectsSection";
+import ProjectItems from "../components/list/ProjectItems";
 
 const Projects = () => {
   return (
-    <>
-      <ProjectsSection />
-    </>
+    <section className="mx-5 mt-14 md:mx-14 md:mt-20 lg:mx-36">
+      <h1 className="text-xl font-semibold md:text-3xl">Projects</h1>
+      <ProjectItems />
+    </section>
   );
 };
 
