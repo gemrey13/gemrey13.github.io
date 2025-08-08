0a4aacdb4871681af8d996a9d124f08867a10fd1
gemrey13 <gemreyranola@gmail.com>
2025-08-08 11:40:36 +0800

    feat: refactor routing by removing routes.tsx and integrating routes directly in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,22 @@
-import { RouterProvider } from "react-router-dom";
 import { createRoot } from "react-dom/client";
-import { router } from "./routes";
 import "./assets/css/index.css";
 import { HelmetProvider } from "react-helmet-async";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import MainLayout from "./layout/MainLayout";
+import Projects from "./pages/Projects";
+import AboutMe from "./pages/AboutMe";
+import Home from "./pages/Home";
 
 createRoot(document.getElementById("root")!).render(
   <HelmetProvider>
-    <RouterProvider router={router} />
+    <BrowserRouter>
+      <Routes>
+        <Route element={<MainLayout />}>
+          <Route index element={<Home />} />
+          <Route path="projects" element={<Projects />} />
+          <Route path="about-me" element={<AboutMe />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   </HelmetProvider>,
 );
